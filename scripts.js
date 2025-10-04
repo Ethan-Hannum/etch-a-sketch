1c8b297416cd74b76f0084902e570589d96ac215
IknuDeer <ethanhannum10@gmail.com>
2025-10-04 15:15:59 -0700

    Set width of each div in for loop to divWidth
    divWidth is the size of the container, 960px, divided by the user input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const container = document.querySelector("#container");
+// Values for what the user will input and the width/height of a div
+let userInput = 16;
+let divWidth = 960 / userInput;
 // Loop to create X amount of "square" divs for the grid
-for (i = 1; i <= 16; ++i) {
+for (i = 1; i <= 256; ++i) {
     const div = document.createElement("div");
     div.setAttribute("id", `div${i}`);
     div.classList.add("grid");
+    div.setAttribute("style", `width: ${divWidth}px`)
     container.appendChild(div);
 }
 
