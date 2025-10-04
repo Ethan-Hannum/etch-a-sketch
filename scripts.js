74e3835496d1833d0ac5a78fee52f46fdea4d55b
IknuDeer <ethanhannum10@gmail.com>
2025-10-04 16:45:52 -0700

    Make divs change color when hovered
    Event listeners added in during the grid creation loop listen for
    the mouse to enter the div. Once so the div changes color.
    Also made it so the amount of divs generated is dependent on userInput

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 const container = document.querySelector("#container");
 // Values for what the user will input and the width/height of a div
 let userInput = 16;
+let totalSquares = userInput ** 2;
 let divWidth = 960 / userInput;
+
+// When mouse enters div, change it's color
+function changeColor(e) {
+    e.target.style.background = "blue";
+}
+
 // Loop to create X amount of "square" divs for the grid
-for (i = 1; i <= 256; ++i) {
+for (i = 1; i <= totalSquares; ++i) {
     const div = document.createElement("div");
     div.setAttribute("id", `div${i}`);
     div.classList.add("grid");
-    div.setAttribute("style", `width: ${divWidth}px`)
+    div.setAttribute("style", `width: ${divWidth}px;`)
+    div.addEventListener("mouseenter", changeColor);
     container.appendChild(div);
 }
 
